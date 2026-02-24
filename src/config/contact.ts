dc32f9d7526eac2ac7560b25aa9e31e79a5fddb7
j0shcd <joshua@cohendumani.com>
2026-02-24 21:18:47 +0100

    add real email

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -4,4 +4,4 @@
  * Currently using test email for development.
  * For production, change to: plecordier@free.fr
  */
-export const CONTACT_EMAIL = 'lecordier.flore@gmail.com';
+export const CONTACT_EMAIL = 'plecordier@free.fr';
